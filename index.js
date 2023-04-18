dba28bee8f69bf8c70f3805873580ca3d913f0e8
Jet <jessicarturner00@gmail.com>
2023-04-19 00:25:01 +0800

    Added basic logic to game

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+
 let firstCard = 10
 let secondCard = 11
-// 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
+let message = ""
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+    message = "Blackjack!"
+    hasBlackjack = true
+} else {
+    message = "You're out of the game"
+    isAlive = false
+}
+
+console.log(message)
+
 
