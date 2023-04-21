ba47e8a11116c551c4be44e2171cf265bf70bb8e
Jet <jessicarturner00@gmail.com>
2023-04-21 23:56:36 +0800

    Added gameplay functionality
    added start and new card buttons

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 
 let firstCard = 10
 let secondCard = 11
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Blackjack!"
-    hasBlackjack = true
-} else {
-    message = "You're out of the game"
-    isAlive = false
+function startGame(){
+    refreshGame()
 }
 
-console.log(message)
+function refreshGame(){
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Blackjack!"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    console.log("drawing a new card from the deck")
+    let card = 2
+    sum += card
+    refreshGame()
+}
+
+
 
 
