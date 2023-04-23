6d4db8cd257738ab0411881c27d6ce8610da4b4b
Jet <jessicarturner00@gmail.com>
2023-04-23 15:49:30 +0800

    Added functionality to generate random cards and display them

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-let firstCard = 10
-let secondCard = 11
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackjack = false
@@ -10,12 +10,19 @@ let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el")
 
+function getRandomCard(){
+    return Math.floor((Math.random() * 11) + 1)
+}
+
 function startGame(){
     refreshGame()
 }
 
 function refreshGame(){
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -31,10 +38,16 @@ function refreshGame(){
 
 function newCard(){
     console.log("drawing a new card from the deck")
-    let card = 2
-    sum += card
+    let card = getRandomCard()
+    cards.push(card)
+    let total = 0
+    for (let i = 0; i < cards.length; i++) {
+        total += cards[i]
+    }
+    sum = total
     refreshGame()
 }
+
 
 
 
